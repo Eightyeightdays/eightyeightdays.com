d7193b1b94f9a605e98e2f572b5332f088293925
Eightyeightdays <eightyeightdays@gmail.com>
2022-12-15 17:05:50 +0100

    Basic header and footer in layout component added to Gatsby project

---
+++ eightyeightdays-gatsby/src/components/layout/Layout.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "./Header.js"
+import Footer from "./Footer.js"
+
+export default function Layout({ children }) {
+  return (
+    <>
+        <Header/>
+            {children}
+        <Footer />
+    </>
+  )
+}
---
+++ eightyeightdays-gatsby/src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import Layout from "../components/layout/Layout"
+
+const IndexPage = () => {
+  return (
+    <Layout >
+      <div>Home Page</div>
+    </Layout>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
