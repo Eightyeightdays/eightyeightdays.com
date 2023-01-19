b17b0a0479129622ad9f54a3707037269fa0c512
Eightyeightdays <eightyeightdays@gmail.com>
2023-01-19 18:02:22 +0100

    Menu and submenu can now be accessed via click for mobile devices

--- Eightyeightdays-Next/components/Menu.js
+++ Eightyeightdays-Next/components/Menu.js
@@ -1,22 +1,35 @@
 import styles from "../styles/Menu.module.css"
 import Link from "next/link"
 
-export default function Menu({toggle}){
-    console.log(toggle)
+export default function Menu({toggle, toggleMenu, subToggle, toggleSubMenu, setSubToggle}){
+
+    if(!toggle){
+        setSubToggle(false)
+    }
+
     return(
-        <div className={styles.menu}>
-            <Link className={`${styles.menu_title} ` + `${styles.menu_title_1} ` + (toggle ? `${styles.menu_title_clicked}` : "")} href="/about/">Pars Pro Toto</Link>
-            <Link className={`${styles.menu_title} ` + `${styles.menu_title_2} ` + (toggle ? `${styles.menu_title_clicked}` : "")} href="/human-writes/">Human Writes</Link>
-            <Link className={`${styles.menu_title} ` + `${styles.menu_title_3} ` + (toggle ? `${styles.menu_title_clicked}` : "")} href="/non-martial-arts/">Non Martial Arts</Link>
-            <Link className={`${styles.menu_title} ` + `${styles.menu_title_4} ` + (toggle ? `${styles.menu_title_clicked}` : "")} href="/exhibitionism/">Indecent Exposures</Link>
-            <div className={styles.menu_dropdown}>
-                <div><Link className={`${styles.menu_dropdown_link} ` + `${styles.menu_title_5} ` + (toggle ? `${styles.menu_dropdown_link_clicked}` : "")} href="/an-eye-for-an-eye/">An Eye For An Eye</Link></div>
-                <div className={styles.menu_content}>
-                    <Link className={styles.menu_content_item} href="/an-eye-for-an-eye/illustration">Illustration</Link>
-                    <Link className={styles.menu_content_item} href="/an-eye-for-an-eye/photosynthesis">Photosynthesis</Link>
-                    <Link className={styles.menu_content_item} href="/an-eye-for-an-eye/on-photography">On Photography</Link>
-                    <Link className={styles.menu_content_item} href="/an-eye-for-an-eye/videos">Succession Stories</Link>  
+        <div className={styles.container}>
+            <div className={styles.menu} onClick={toggleMenu}>
+                <Link className={`${styles.menu_title} ` + `${styles.delay_1} ` + (toggle ? `${styles.clicked}` : "")} href="/about/">Pars Pro Toto</Link>
+                <Link className={`${styles.menu_title} ` + `${styles.delay_2} ` + (toggle ? `${styles.clicked}` : "")} href="/human-writes/">Human Writes</Link>
+                <Link className={`${styles.menu_title} ` + `${styles.delay_3} ` + (toggle ? `${styles.clicked}` : "")} href="/non-martial-arts/">Non Martial Arts</Link>
+                <Link className={`${styles.menu_title} ` + `${styles.delay_4} ` + (toggle ? `${styles.clicked}` : "")} href="/exhibitionism/">Indecent Exposures</Link>
+                <div className={styles.menu_dropdown}>
+                    <div>
+                        <Link className={`${styles.menu_dropdown_link} ` + `${styles.delay_5} ` + (toggle ? `${styles.clicked}` : "")} href="/an-eye-for-an-eye/">
+                            An Eye For An Eye
+                        </Link>
+                    </div>
+                    <div className={`${styles.menu_content} ` + (subToggle ? `${styles.menu_content_clicked}` : "")}>
+                        <Link className={styles.menu_content_item} href="/an-eye-for-an-eye/illustration">Illustration</Link>
+                        <Link className={styles.menu_content_item} href="/an-eye-for-an-eye/photosynthesis">Photosynthesis</Link>
+                        <Link className={styles.menu_content_item} href="/an-eye-for-an-eye/on-photography">On Photography</Link>
+                        <Link className={styles.menu_content_item} href="/an-eye-for-an-eye/videos">Succession Stories</Link>  
+                    </div>
                 </div>
+            </div>
+            <div className={styles.test}>
+                <div className={`${styles.test_button} ` + (subToggle ? `${styles.test_button_clicked}` : "")} onClick={toggleSubMenu}>{subToggle ? "X" : "Y"}</div>
             </div>
         </div>
     )
--- Eightyeightdays-Next/components/layout/Header.js
+++ Eightyeightdays-Next/components/layout/Header.js
@@ -4,22 +4,27 @@ import Menu from "../Menu.js"
 import { useState } from "react"
 
 export default function Header(){
-    const [menu, setMenu] = useState(false);
+    const [toggle, setToggle] = useState(false);
+    const [subToggle, setSubToggle] = useState(false);
 
     function toggleMenu(){
-        setMenu(!menu)
+        setToggle(!toggle)
+    }
+
+    function toggleSubMenu(){
+        setSubToggle(!subToggle)
     }
 
     return(
         <>
             <header id="header" className={styles.header}>
                 <div className={styles.header_title}>
-                    <Link href="/" onClick={toggleMenu}>Eightyeightdays</Link>
+                    <Link href="/">Eightyeightdays</Link>
                 </div>
-                <div className={menu ? styles.menu_container_clicked : styles.menu_container} onClick={toggleMenu}>
-                    <Menu toggle={menu}/>
+                <div className={toggle ? styles.menu_container_clicked : styles.menu_container} >
+                    <Menu toggle={toggle} toggleMenu={toggleMenu} subToggle={subToggle} toggleSubMenu={toggleSubMenu} setSubToggle={setSubToggle}/>
                 </div>
-                <div className={menu? styles.menu_button_clicked : styles.menu_button} onClick={toggleMenu}>{menu? "X" : "Y"}</div>
+                <div className={`${styles.menu_button} ` + (toggle? `${styles.menu_button_clicked}` : "")} onClick={toggleMenu}>{toggle? "X" : "Y"}</div>
             </header>
         </>
     )
