01c84a732d17bc597bd9076065c93a5b4a7cfea2
Eightyeightdays <eightyeightdays@gmail.com>
2023-05-02 16:17:41 +0200

    Styles added for wordpress classes

--- front/pages/TEST.js
+++ front/pages/TEST.js
@@ -1,8 +1,27 @@
+import parse from "html-react-parser";
+import Head from "next/head";
+import styles from "../styles/TEST.module.css"
+
 export async function getStaticProps(){
-   const data = await fetch("http://eightyeightdays.com/wp-json/wp/v2/posts") ;
+   const json = await fetch("http://eightyeightdays.com/wp-json/wp/v2/posts?_fields=title,content,yoast_head") ;
+   const data = await json.json()
    return {props:{data}}
 }
 
-export default function Test({props}){
-    console.log(props);
+export default function Test({data}){
+    const title = parse(data[0].title.rendered)
+    const content = parse(data[0].content.rendered);
+    const head = parse(data[0]["yoast_head"]);
+
+    return(
+        <>
+            <Head>
+                {head}
+            </Head>
+            <div className={styles.container}>
+                <h1>{title}</h1>
+                {content}
+            </div>
+        </>
+    )
 }
