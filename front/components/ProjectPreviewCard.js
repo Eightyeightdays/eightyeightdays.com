6848b11d8a5bc44bd890810537fb982bec9aac49
Eightyeightdays <eightyeightdays@gmail.com>
2023-05-12 11:13:37 +0200

    Solution for highlighting anchor links in post content added, bug when apostrophe in post title fixed with a simple string replace

--- front/components/ProjectPreviewCard.js
+++ front/components/ProjectPreviewCard.js
@@ -2,10 +2,11 @@ import Link from "next/link";
 import styles from "../styles/ProjectPreviewCard.module.css";
 
 export default function ProjectPreviewCard({type, slug, url, alt, title, description, year}){
+    const newTitle = title.replace("&#8217;", "'");
     return(
         <Link className={styles.preview_card} href={`/an-eye-for-an-eye/${type}/${slug}`}>
             <img className={styles.preview_card_img} src={url} alt={alt} />
-            <div className={styles.preview_card_title}>{title}</div>
+            <div className={styles.preview_card_title}>{newTitle}</div>
             <div className={styles.preview_card_description}>{description}</div>
             <div className={styles.symbol_container}>&#9830;</div>
             <div className={styles.preview_card_year}>{year}</div>
--- front/components/VisualProject.js
+++ front/components/VisualProject.js
@@ -6,7 +6,8 @@ import styles from "../styles/VisualProject.module.css"
 export default function VisualProject({project}){
     const content = parse(project.content.rendered);
     const dataType = project.acf.type.replace(/-/g, " ");
-
+    const oldTitle = project.title.rendered;
+    const title = oldTitle.replace("&#8217;", "'");
     return(
         <>
             <Head>
@@ -17,10 +18,11 @@ export default function VisualProject({project}){
             <div className={styles.outer_container}>
                 <Link className={styles.navigate_button} href={`/an-eye-for-an-eye?category=${project.acf.type}#previewContainer`}>Back to <div className={styles.dataType}>{dataType}</div></Link>
                 <div className={styles.main_container}>
-                    <h1 className={styles.title}>{project.title.rendered}</h1>
+                    <h1 className={styles.title}>{title}</h1>
                     <img src={project.acf.banner.url} alt={project.acf.banner.alt} className={styles.main_img}></img>
                     <div className={styles.divider}></div>
                     <div className={styles.content}>{content}</div>
+                    <div className={styles.symbol_container}>&#9830; &#9830; &#9830;</div>
                 </div>
             </div>
         </>
